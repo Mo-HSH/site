bc2451122d3eddf4dc4153fed68bd77ca6038c32
= <kamyabzad@gmail.com>
2025-02-01 13:49:52 +0330

    run's absence date and run's run date output

--- src/routes/soldier/ListSoldier.jsx
+++ src/routes/soldier/ListSoldier.jsx
@@ -272,6 +272,38 @@ function ListSoldier() {
             },
         },
         {
+            label: "تاریخ فرار",
+            value: {
+                title: "تاریخ فرار",
+                dataIndex: "run",
+                render: ((run)=>{
+                    try {
+                        let dates = run.sort((a, b)=> a.run_date.$date.$numberLong - b.run_date.$date.$numberLong);
+                        return DateRenderer(dates[dates.length - 1].run_date);
+                    }
+                    catch {
+                        return " ";
+                    }
+                })
+            },
+        },
+        {
+            label: "تاریخ نهست فرار",
+            value: {
+                title: "تاریخ نهست فرار",
+                dataIndex: "run",
+                render: ((run)=>{
+                    try {
+                        let dates = run.sort((a, b)=> a.absence_date.$date.$numberLong - b.absence_date.$date.$numberLong);
+                        return DateRenderer(dates[dates.length - 1].absence_date);
+                    }
+                    catch {
+                        return " ";
+                    }
+                })
+            },
+        },
+        {
             label: "تاریخ ترخیص",
             value: {
                 title: "تاریخ ترخیص",
